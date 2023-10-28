7777caade1356a6e39488c9e1bf919c63d7b901e
hugetuna <113012817+hugetuna@users.noreply.github.com>
2023-10-28 23:37:02 +0800

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -8,4 +8,23 @@ $(document).ready(function () {
             $(this).children('.nav-ul-l2').slideUp(200);
         }
     );
+
+    $(window).scroll(
+        function () {
+            var scroll = $(window).scrollTop(); // 取得滾動條位置
+            if (scroll > 1000) {
+                $('.intro-img').children('#img1').fadeIn(500);
+            }
+            if (scroll > 1400) {
+                $('.intro-img').children('#img2').fadeIn(500);
+            }
+            if (scroll > 1800) {
+                $('.intro-img').children('#img3').fadeIn(500);
+            }
+            if (scroll < 1000) {
+                $('.intro-img').children('img').fadeOut(200);
+            }
+
+        }
+    );
 });
